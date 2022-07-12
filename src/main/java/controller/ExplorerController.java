5162fba2e9ece6d40324b46374351617b1038e75
samrands <samuel.rands@aggiemail.usu.edu>
2022-07-12 17:28:51 -0600

    Adding basic controller logic

--- src/main/java/controller/ExplorerController.java
+++ src/main/java/controller/ExplorerController.java
@@ -2,7 +2,11 @@ package controller;
 
 import model.ExplorerModel;
 
+import java.io.File;
+import java.util.Map;
+
 public class ExplorerController {
+    private Map<String, File> namesToFiles;
 
     private final ExplorerModel explorerModel;
     private final ExplorerView explorerView;
@@ -10,6 +14,28 @@ public class ExplorerController {
     public ExplorerController(ExplorerModel explorerModel, ExplorerView explorerView) {
         this.explorerModel = explorerModel;
         this.explorerView = explorerView;
+    }
+
+    public void createDirectory(String directoryName) throws Exception {
+        explorerModel.createDirectory(directoryName);
+    }
+
+    public void changeDirectory(File file) throws Exception {
+        explorerModel.changeDirectory(file.getAbsolutePath());
+
+        refreshFiles();
+    }
+
+    public void moveUpDirectory() throws Exception {
+        explorerModel.moveUpDirectory();
+
+        refreshFiles();
+    }
+
+    private void refreshFiles() {
+        File[] updatedFiles = explorerModel.getChildren();
+
+        explorerView.updateFiles(updatedFiles);
     }
 
 
